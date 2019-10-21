d608cdcc86ee0274cabb86eaba6bd54f569aecf2
Frank de Groot - Schouten <fjtdg@hotmail.com>
2019-10-21 21:39:32 +0200

    Done until templating

---
+++ README.md
@@ -0,0 +1,3 @@
+# snap-hello-world
+
+[This](https://usethetypes.com) tutorial combined with HTF.
---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Applicative ((<|>))
+import Snap.Core (ifTop, route)
+import Snap.Http.Server (httpServe, setPort)
+import Snap.Util.FileServe (serveDirectory, serveFile)
+import System.Environment (lookupEnv)
+
+main :: IO ()
+main = do
+    mbPort <- lookupEnv "PORT"
+    let port = maybe 8000 read mbPort
+        config = setPort port mempty
+    httpServe config $
+        ifTop (serveFile "views/index.html") <|>
+        route [("/static", serveDirectory "static")]
+        <|> serveFile "views/notfound.html"
